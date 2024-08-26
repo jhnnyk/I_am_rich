21e4634e805053ec2129acd1c2f9eb0478324d23
John Knotts <jhnnyk@gmail.com>
2024-08-26 13:17:49 -0600

    using image assets

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,7 @@ void main() {
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage(
-                'https://media.wired.com/photos/65382632fd3d190c7a1f5c68/16:9/w_2400,h_1350,c_limit/Google-Image-Search-news-Gear-GettyImages-824179306.jpg'),
+            image: AssetImage('images/diamond.png')
           ),
         ),
       ),
